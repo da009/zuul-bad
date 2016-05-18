f94e43f8679f39dc2cd6217087b271115459e1ba
Daniel Pradera <Daniel Pradera>
2016-05-18 12:24:39 +0200

    archivo .jar

---
+++ WorldOfZuul.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class WorldOfZuul here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class WorldOfZuul
+{
+    /**
+     * Constructor for objects of class WorldOfZuul
+     */
+    public static void main(String [] args)
+    {
+        Game ng;
+        ng = new Game(15);
+        ng.play();
+    }
+}
